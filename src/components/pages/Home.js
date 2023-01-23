cee61f287b65964205aca4fc3e0de5d4ca07cdee
José Leyva <jrleyva41096@gmail.com>
2023-01-23 16:07:38 -0500

    rearranged text to look more neat

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,7 +8,13 @@ export default function Home() {
         Hello! I'm an amatuer web developer interested in the tech industry and gaming.
       </h1>
       <h2>How I got started</h2>
-      <p>I began this journey by signing up for a full-time 90 day bootcamp course provided by the Univercity of Central Florida. I had NO prior knowledge of coding, and only had a surface/hobbyist level of knowledge of computers. The bootcamp started simple in the first month with html and CSS, and light javascript. I felt confident that first month; but was immediately humbled going into the second month with the introduction of more advanced JavaScript, databases with DBeaver, and node. The third month felt like the longest month, </p>
+      <p>I began this journey by signing up for a full-time 90 day bootcamp course provided
+        by the Univercity of Central Florida. I had NO prior knowledge of coding, and only
+        had a surface/hobbyist level of knowledge of computers. The bootcamp started simple
+        in the first month with html and CSS, and light javascript. I felt confident that
+        first month; but was immediately humbled going into the second month with the
+        introduction of more advanced JavaScript, databases with DBeaver, and node. The third
+        month felt like the longest month, </p>
       <h2>What led me to this interest?</h2>
       <p>blalblablablablablablabla</p>
       <h2>What do I do for fun?</h2>
